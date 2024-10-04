f7a4c9dd7f0b17b68c18e53d5e734a435d01c803
Jackson Mena <mena.jack72@gmail.com>
2024-10-03 22:21:51 -0300

    criação class mãe Conteudo

--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -3,23 +3,17 @@ package br.com.dio.desafio.dominio;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-public class Mentoria {
-    private String titulo;
-    private String descricao;
+public class Mentoria extends Conteudo {
+
     private LocalDate data;
     
-    public String getTitulo() {
-        return titulo;
+    public Mentoria() {
     }
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
+
+    public double calcularXp() {
+        return XP_PADRAO + 20d;
     }
-    public String getDescricao() {
-        return descricao;
-    }
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
+
     public LocalDate getData() {
         return data;
     }
@@ -28,7 +22,7 @@ public class Mentoria {
     }
     @Override
     public String toString() {
-        return "Mentoria [titulo=" + titulo + ", descricao=" + descricao + ", data=" + formatarData() + "]";
+        return "Mentoria [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", data=" + formatarData() + "]";
     }
 
     public String formatarData() {
