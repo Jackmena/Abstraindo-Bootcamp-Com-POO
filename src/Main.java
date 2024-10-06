1e88dec7b9bddbe2ab58879cc624e82f2f2e7abf
Jackson Mena <mena.jack72@gmail.com>
2024-10-06 15:39:09 -0300

    Testes na class Main e concluindo o desafio

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.time.LocalDate;
 
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
@@ -15,8 +17,37 @@ public class Main {
         mentoria.setDescricao("Descrição mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(mentoria);
+        //System.out.println(curso1);
+        //System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devJackson = new Dev();
+        devJackson.setNome("Jackson");
+        devJackson.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Jackson" + devJackson.getConteudosInscritos());
+        devJackson.progredir();
+        System.out.println("--");
+        System.out.println("Conteúdos Inscritos Jackson" + devJackson.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Jackson" + devJackson.getConteudosConcluidos());
+        System.out.println("XP: " + devJackson.calcularTotalXp());
+        
+        System.out.println("---------");
+        
+        Dev devDani = new Dev();
+        devDani.setNome("Danillo");
+        devDani.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Danillo" + devDani.getConteudosInscritos());
+        devDani.progredir();
+        devDani.progredir();
+        System.out.println("--");
+        System.out.println("Conteúdos Inscritos Danillo" + devDani.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Danillo" + devDani.getConteudosConcluidos());
+        System.out.println("XP: " + devDani.calcularTotalXp());
     }
 
 }
